95a9f166410fbe5597d373902444399be4e83f77
Bruno Rybicki <175746513+brunorybicki@users.noreply.github.com>
2026-08-20 20:35:15 +0200

    Add solutions for 69. Sqrt(x)

---
+++ 69. Sqrt(x)/sqrtx.c
@@ -0,0 +1,13 @@
+int mySqrt(int x) {
+    if (x < 2) {
+        return x;
+    }
+
+    long result = 1;
+
+    while (result * result <= x) {
+        result++;
+    }
+
+    return (int)(result - 1);
+}
