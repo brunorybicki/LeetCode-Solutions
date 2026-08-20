bd5b49cb0223576ca346dce8fce02589172b921b
Bruno Rybicki <175746513+brunorybicki@users.noreply.github.com>
2026-08-20 20:44:40 +0200

    Add solutions for 70. Climbing Stairs

---
+++ 70. Climbing Stairs/climbingstairs.c
@@ -0,0 +1,11 @@
+int climbStairs(int n) {
+    int previous = 1, current = 1;
+
+    for (int i = 2; i <= n; i++) {
+        int next = previous + current;
+        previous = current;
+        current = next;
+    }
+    
+    return current;
+}
