9253aa3c0370fbe135890c17b9edaa0052e8ec00
Bruno Rybicki <175746513+brunorybicki@users.noreply.github.com>
2026-09-25 04:08:07 +0200

    Add solutions for 278. First Bad Version

---
+++ 278. First Bad Version/firstbadversion.c
@@ -0,0 +1,18 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+int firstBadVersion(int n) {
+    int left = 1, right = n;
+
+    while (left < right) {
+        int middle = left + (right - left) / 2;
+
+        if (isBadVersion(middle)) {
+            right = middle;
+        } else {
+            left = middle + 1;
+        }
+    }
+
+    return left;
+}
